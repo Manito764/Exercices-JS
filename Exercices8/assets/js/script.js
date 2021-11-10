e95389cc431c9f31551b5f388dff6a1c69dd7d6d
Manito764 <93980103+Manito764@users.noreply.github.com>
2021-11-10 17:11:02 +0100

    Fin Exercice 8

---
+++ Exercices8/assets/js/script.js
@@ -0,0 +1,8 @@
+var pika=document.getElementById('pika');
+
+pika.addEventListener("mouseenter", function change(){
+document.getElementById("pika").src="./assets/img/026.png";
+})
+pika.addEventListener("mouseout", function change(){
+    document.getElementById("pika").src="./assets/img/025.png";
+})
