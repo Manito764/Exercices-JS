e8ad1772b8ee40c1ccde0ecd7b24bc2aeaa93dff
Manito764 <93980103+Manito764@users.noreply.github.com>
2021-11-16 11:55:22 +0100

    correction

--- Exercices9/assets/js/script.js
+++ Exercices9/assets/js/script.js
@@ -1,5 +1,22 @@
+//Création de la variable image
 var pika=document.getElementById('pika');
+var initwitdhpika = pika.clientWidth
 
-pika.addEventListener("click", function grow(){
-    pika.style.transform = "scale(1.5)";
-})
+// création de la fonction click pour intéragir au click
+
+pika.addEventListener('click', function toto (){
+// création d'une variable pour récuperer la largeur de l'image
+var witdhpika = pika.clientWidth;
+// Création d'une condition. Si la largeur de l'image est supérieure a 1500px
+if (witdhpika>1500) {
+// Alors elle reviens à sa taille d'origine
+    pika.style.width = (initwitdhpika+'px')
+    //sinon, elle agrandie sa taille de 10%
+} else {
+    pika.style.width = (witdhpika*1.1+'px')
+}
+});
+
+console.log(initwitdhpika);
+//Merci et a bientot :)
+
